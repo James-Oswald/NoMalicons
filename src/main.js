25b1726ba1efb04250eb849783dc6a0aea08f62b
James Oswald <jamesoswald111@gmail.com>
2021-12-27 03:49:38 -0500

    fixed matchs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 
 function run(){
     let malicons = document.getElementsByClassName("malicon");
-    for(mailcon of mailicons)
+    for(mailcon of malicons)
         mailcon.style.visibility="hidden";
+    console.log("Removed Malicons");
 }
 
+console.log("NoMalicons Injected!");
 run();
