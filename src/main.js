1431f5968e8aff39f119ee82166288103bb4130b
James Oswald <jamesoswald111@gmail.com>
2022-01-08 03:10:50 -0500

    moved the status back to where it should be

--- src/main.js
+++ src/main.js
@@ -1,20 +1,36 @@
 
 function run(){
+    //get rid of those cringe previews
     let malicons = document.getElementsByClassName("malicon");
     let numMalicons = 0;
-    for(mailcon of malicons){
+    for(let mailcon of malicons){
         numMalicons++;
         mailcon.style.visibility="hidden";
     }
     console.log("Removed" + numMalicons + " Malicons");
+
+    //Give that a nice color
     let addToListBtns = document.getElementsByClassName("addtolist");
     let numBtns = 0;
-    for(addToListBtn of addToListBtns){
+    for(let addToListBtn of addToListBtns){
         numBtns++;
-        addToListBtn.backgroundColor="white";
+        addToListBtn.style.backgroundColor="white";
         addToListBtn.style.color = "gray";
     }
     console.log("Edited " + numBtns + " Button Styles");
+
+    //Put those buttons up top where they belong
+    
+    let seasonalAnimeBoxs = document.getElementsByClassName("seasonal-anime");
+    for(let seasonalAnimeBox of seasonalAnimeBoxs){
+        statusBox = seasonalAnimeBox.children[3].children[1];
+        seasonalAnimeBox.children[0].children[1].children[1].appendChild(statusBox);
+    }
+
+    let bubbles = document.getElementsByClassName("bubble");
+    for(let bubble of bubbles){
+        bubble.style.visibility="hidden";
+    }
 }
 
 console.log("NoMalicons Injected!");
