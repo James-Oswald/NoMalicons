02d2c42a38c42de63b1f90d4c906cb198c26f157
James Oswald <jamesoswald111@gmail.com>
2022-01-08 02:49:37 -0500

    added add to list button color swap

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@
 # No Malicons!
 
 Removes the annoying bright blue icons on the MAL seasonal page that make it hard to see your show status.
+
+Now also gives better colors to the add list
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 
 function run(){
     let malicons = document.getElementsByClassName("malicon");
-    for(mailcon of malicons)
+    let numMalicons = 0;
+    for(mailcon of malicons){
+        numMalicons++;
         mailcon.style.visibility="hidden";
-    console.log("Removed Malicons");
+    }
+    console.log("Removed" + numMalicons + " Malicons");
+    let addToListBtns = document.getElementsByClassName("addtolist");
+    let numBtns = 0;
+    for(addToListBtn of addToListBtns){
+        numBtns++;
+        addToListBtn.backgroundColor="white";
+        addToListBtn.style.color = "gray";
+    }
+    console.log("Edited " + numBtns + " Button Styles");
 }
 
 console.log("NoMalicons Injected!");
